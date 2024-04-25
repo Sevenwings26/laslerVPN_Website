dcb5e3b9edf2072166e89e3c87fd75fd849cfbe3
Sevenwings26 <iarowosola@yahoo.com>
2024-04-25 05:54:30 +0100

    Nav completed

---
+++ Assets/JS/scripts.js
@@ -0,0 +1,11 @@
+const toggleDisplay = document.getElementById('togg-display');
+
+function toggle(){
+    if (toggleDisplay.style.display = 'none'){
+        toggleDisplay.style.display = 'block';
+    } 
+}
+
+function cancel(){
+    toggleDisplay.style.display = 'none';
+}
