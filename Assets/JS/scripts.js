1d6853ef7244fb7d14fe264665a7cffa3b7902e0
Sevenwings26 <iarowosola@yahoo.com>
2024-04-26 03:06:02 +0100

    Upload works

--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -1,4 +1,13 @@
+// get toggle id 
 const toggleDisplay = document.getElementById('togg-display');
+
+// get modal id 
+const modal = document.getElementById('register');
+const modalLogin = document.getElementById('login-id');
+
+const closeModal = document.getElementById('close-signup');
+let closeModalLogin = document.getElementById('close-login');
+
 
 function toggle(){
     if (toggleDisplay.style.display = 'none'){
@@ -9,3 +18,25 @@ function toggle(){
 function cancel(){
     toggleDisplay.style.display = 'none';
 }
+
+
+function signup(){
+    if (modal.style.display = 'none'){
+        modal.style.display = 'block';
+    }
+}
+
+function login(){
+    if (modalLogin.style.display = 'none'){
+        modalLogin.style.display = 'block';
+    }
+};
+
+closeModal.addEventListener('click', (e) => {
+    modal.style.display = 'none';
+});
+
+closeModalLogin.addEventListener('click', (e) => {
+    modalLogin.style.display = 'none';
+});
+
