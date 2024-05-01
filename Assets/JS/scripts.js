233555bb33cef5c79f5886e362503f4cbfdfe30b
Sevenwings26 <iarowosola@yahoo.com>
2024-05-01 18:07:44 +0100

    Update work

--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -40,3 +40,23 @@ closeModalLogin.addEventListener('click', (e) => {
     modalLogin.style.display = 'none';
 });
 
+
+
+// slider 
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
